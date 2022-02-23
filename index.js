5f95c53e684ebeeea90671e9e8702e27e0410df7
Tomi <tomiwest@hotmail.com>
2022-02-23 17:42:06 +0200

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,95 +1,82 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.static('build'))
-
 morgan.token('body', (req) => JSON.stringify(req.body))
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-
-  const person = persons.find((person) => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-app.get('/info', (request, response) => {
-  const count = persons.length
-  const date = new Date()
-
-  response.send(`<p>Phonebook has info for ${count} people</p>
-  <p> ${date} </p>`)
+  console.log('Get persons from Mongo Db')
+  Person.find({}).then((persons) => {
+    console.log('Get Data from MongoDB')
+    response.json(persons)
+  })
+  //mongoose.connection.close()
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
+// app.get('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
 
-  response.status(204).end()
-})
-const generateId = () => {
-  const id = Math.floor(Math.random() * 100) + 1
-  return id
-}
-app.post('/api/persons/', morgan(':body'), (request, response) => {
-  const body = request.body
+//   const person = persons.find((person) => person.id === id)
 
-  if (!body.name || !body.number) {
-    console.log(body)
-    return response.status(400).json({
-      error: 'name or number is missing',
-    })
-  }
-  const person = persons.find((person) => person.name === body.name)
+//   if (person) {
+//     response.json(person)
+//   } else {
+//     response.status(404).end()
+//   }
+// })
 
-  if (!person) {
-    let person = {
-      id: generateId(),
-      name: body.name,
-      number: body.number,
-    }
-    persons = persons.concat(person)
-    response.json(person)
-  } else {
-    return response.status(400).json({
-      error: 'name must be unique',
-    })
-  }
-})
+// app.get('/info', (request, response) => {
+//   const count = persons.length
+//   const date = new Date()
+
+//   response.send(`<p>Phonebook has info for ${count} people</p>
+//   <p> ${date} </p>`)
+// })
+
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   persons = persons.filter((person) => person.id !== id)
+
+//   response.status(204).end()
+// })
+// const generateId = () => {
+//   const id = Math.floor(Math.random() * 100) + 1
+//   return id
+// }
+// app.post('/api/persons/', morgan(':body'), (request, response) => {
+//   const body = request.body
+
+//   if (!body.name || !body.number) {
+//     console.log(body)
+//     return response.status(400).json({
+//       error: 'name or number is missing',
+//     })
+//   }
+//   const person = persons.find((person) => person.name === body.name)
+
+//   if (!person) {
+//     let person = {
+//       id: generateId(),
+//       name: body.name,
+//       number: body.number,
+//     }
+//     persons = persons.concat(person)
+//     response.json(person)
+//   } else {
+//     return response.status(400).json({
+//       error: 'name must be unique',
+//     })
+//   }
+// })
 const PORT = process.env.PORT || 3001 //
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
